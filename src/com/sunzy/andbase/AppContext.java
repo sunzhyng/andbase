b67280c8f592101180ccf918d3edb0dec1305f2b
sunzhyng <sunzhyng@sina.com>
2014-08-22 17:55:17 +0800

    appmanage & appexception

--- src/com/sunzy/andbase/AppContext.java
+++ src/com/sunzy/andbase/AppContext.java
@@ -4,24 +4,27 @@ import java.util.Locale;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
 public class AppContext extends Application {
-	
+
 	public static final int NETTYPE_NO = 0x00;
 	public static final int NETTYPE_WIFI = 0x01;
 	public static final int NETTYPE_CMWAP = 0x02;
 	public static final int NETTYPE_CMNET = 0x03;
-	
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
-        
+
 	}
-	
+
 	/**
 	 * Check Network connected
+	 * 
 	 * @return
 	 */
 	public boolean isNetworkConnected() {
@@ -29,10 +32,11 @@ public class AppContext extends Application {
 		NetworkInfo ni = cm.getActiveNetworkInfo();
 		return ni != null && ni.isConnectedOrConnecting();
 	}
-	
+
 	/**
 	 * Network Type
-	 * @return 0：NO   1：WIFI   2：WAP    3：NET
+	 * 
+	 * @return 0：NO 1：WIFI 2：WAP 3：NET
 	 */
 	public int getNetworkType() {
 		int netType = NETTYPE_NO;
@@ -40,12 +44,12 @@ public class AppContext extends Application {
 		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 		if (networkInfo == null) {
 			return netType;
-		}		
+		}
 		int nType = networkInfo.getType();
 		if (nType == ConnectivityManager.TYPE_MOBILE) {
 			Locale loc = Locale.getDefault();
 			String extraInfo = networkInfo.getExtraInfo();
-			if(extraInfo != null && !extraInfo.equals("")){
+			if (extraInfo != null && !extraInfo.equals("")) {
 				if (extraInfo.toLowerCase(loc).equals("cmnet")) {
 					netType = NETTYPE_CMNET;
 				} else {
@@ -56,5 +60,17 @@ public class AppContext extends Application {
 			netType = NETTYPE_WIFI;
 		}
 		return netType;
+	}
+
+	public PackageInfo getPackageInfo() {
+		PackageInfo info = null;
+		try {
+			info = getPackageManager().getPackageInfo(getPackageName(), 0);
+		} catch (NameNotFoundException e) {
+			e.printStackTrace(System.err);
+		}
+		if (info == null)
+			info = new PackageInfo();
+		return info;
 	}
 }
--- src/com/sunzy/andbase/MainActivity.java
+++ src/com/sunzy/andbase/MainActivity.java
@@ -1,24 +1,33 @@
 package com.sunzy.andbase;
 
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Button btn = (Button) findViewById(R.id.button1);
+		btn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "ak", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
-	
-	protected void onResume(){
+
+	protected void onResume() {
 		super.onResume();
-		
+
 		AppContext ac = (AppContext) getApplication();
 		int nt = ac.getNetworkType();
 		TextView txt = (TextView) findViewById(R.id.text_nettype);
-		txt.setText(nt+"");
+		txt.setText(nt + "");
 	}
 }
